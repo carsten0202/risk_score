e216c08f714ffee626f24304c5bfef22f009152a
Carsten Friis Rundsten <carsten.rundsten@gmail.com>
2020-11-05 19:14:48 +0100

    Version 0.4.1
    - Added a test command for testing
    - Sharp2019 under development
    - Testing VCF input
    - VCF input can now be gzipped

---
+++ pkclick.py
@@ -0,0 +1,32 @@
+#!/home/fls530/anaconda3/bin/python
+ 
+import click
+import gzip
+import io
+
+magic_dict = {
+	 b"\x1f\x8b\x08": "gz",
+	 b"\x42\x5a\x68": "bz2",
+	 b"\x50\x4b\x03\x04": "zip"
+}
+
+class gzFile(click.File):
+	def convert(self, value, param, ctx):
+		f = super().convert(value, param, ctx)
+
+		if self._getziptype(f) is not None:
+			gf = io.TextIOWrapper(gzip.GzipFile(fileobj=f))
+			print(gf.readline())
+		sys.exit("Hello")
+		return gf
+
+	@staticmethod
+	def _getziptype(f):
+		if f.seekable():
+			file_start = f.read(max(len(x) for x in magic_dict))
+			f.seek(0)
+			for magic, filetype in magic_dict.items():
+				if file_start.startswith(magic):
+					return filetype
+		return None
+
