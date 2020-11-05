cf1e6c3670b5d14ced078a3b0313f9dd5c2a0362
Carsten Friis Rundsten <carsten.rundsten@gmail.com>
2020-11-05 19:14:48 +0100

    Version 0.6
    - Added a test command for testing
    - Sharp2019 under development
    - VCF input Appears finished in test

--- pkrs.py
+++ pkrs.py
@@ -1,7 +1,7 @@
 #!/home/fls530/anaconda3/envs/snakemake/bin/python
 
 """
-Just like th 're' module; you pre-complie a GRS based on a dict of alt alleles and weights
+Just like the 're' module; you pre-complie a GRS based on a dict of alt alleles and weights
 
 # Pattern are the weights
 # Compile function is the actual algorithm
@@ -9,10 +9,16 @@ Just like th 're' module; you pre-complie a GRS based on a dict of alt alleles a
 
 """
 
+import collections
 import math
 import pathlib
 import pkcsv as csv
 import pksnps
+import sys
+
+#################################################
+#
+# --%%  CLASS: RiskScore  %%--
 
 class RiskScore:
 	"""An algorithm template object for holding the definition of a weight-based risk score.
@@ -20,79 +26,131 @@ class RiskScore:
 	def __init__(self, snps, risks):
 		self.beta   = dict()
 		self.direct = dict()
-		if isinstance(risks, dict):
-			risks = risks.values()
-		self.N = len(risks)
 		if isinstance(snps, dict):
 			snps = snps.values()
 		self.snps   = dict(zip([s.ID for s in snps], snps))   # Dict of SNP instances
-#		raise SystemExit("Unable to process 'factor' argument. Please provide either a list-o-dicts or a dict-o-dicts")
+		risks = self.ReadRisk(risks)
+		self.N = len(risks)
 		for snp in snps:
 			for risk in risks:
 				if snp == risk:
-					self.beta[snp.ID]   = risk.get("BETA",0)
-					self.direct[snp.ID] = risk.get("ALLELE",object()) == snp.ALT
+					self.beta[risk]   = float(risk.get("BETA",0))
+					self.direct[risk] = risk.get("allele",object()) == snp.ALT
 
+# This guy should receive genotypes (Alleles?), or at least be able to...:
 	def calc(self, gtdict):
 		"""This function implements a simple risk score based on a weighted sum.
 		   Subject should be a dict with str(id):float(genotype_score)"""
 		wsum = 0
-		for gtid,score in gtdict.items():
-			wsum += self.beta.get(gtid,0) * abs(float(score) - (0 if self.direct.get(gtid,True) else 2))
+		for gtid,gt in gtdict.items():
+			if isinstance(gt, pksnps.GenoType):
+				for allele in gt.getAllele():
+					wsum += self.beta.get(allele, 0) * allele.p
+			else:
+				sys.exit("ERROR: Looks like you called calc on something thats not a GenoType")
+#				wsum += self.beta.get(gtid,0) * abs(float(gt) - (0 if self.direct.get(gtid,True) else 2))
 		return wsum / self.N
 
+	@staticmethod
+	def ReadRisk(riskfobj):
+		"""Input: A file_obj (Or similar iterator); Return: List of Alleles"""
+		risks = []
+		riskiter = csv.DictReader(riskfobj)
+		for risk in riskiter:
+			chrom = risk.get("CHROM", risk.get("POSID",":").split(":")[0])
+			pos   = risk.get("POS", risk.get("POSID",":").split(":")[1])
+			beta  = risk.get("BETA", math.log(float(risk.get("ODDSRATIO", 1))))
+			try: risks.append(pksnps.Allele(CHROM=str(chrom), POS=int(pos), allele=str(risk.get("ALLELE")), BETA=float(beta)))
+			except AttributeError as ae: 
+				print("\n" + str(ae), file=sys.stderr)
+				sys.exit("Read Error: Each line of '" + str(riskfobj.name) + "' must contain at least weight value with a recognizable position and allele.\n")
+		return risks
+
+#	def calcFromGeno(self, genofobj):
+#		"""Input: File object to a geno file from SNPextractor.py"""
+#		for subject in csv.DictReader(genofobj):
+#			subjectid = subject.pop("")
+#			geno = OrderedDict()
+#			for snpid,value in subject.items():
+#				dosage = sum([float(x) for x in re.split("[/|]+", value)])
+#				geno[snpid] = GenoType(CHROM=info[snpid].CHROM, POS=info[snpid].POS, genotype=info[snpid].getGenoType(dosage), dosage=dosage)
+#			print(subjectid + "\t" + self.calc(geno))
 
 
-class oram2016(RiskScore):
-	"""Calculate GRS based on Oram et al 2016"""
-	def __init__(self, snps, risks=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights.txt", hlafactors=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights_hla.txt", **kwargs):
-		super().__init__(snps=snps, risks=pksnps.ReadRisk(risks) if isinstance(risks, str) else risks, **kwargs)
-		self.hla = self.hlaweights(hlafactors) # self.hla is a dict_dicts using 'GenoType's as keys.
-		self.N = 2 * (self.N + 1)
 
-	def calc_oramhla(self, gtdict):
-		"""Calculate the HLA-part of Oram2016. Gtdict should have the format {str(id):float(allele)}"""
-		wsum = 0
-		gts  = [s.genotype(dosage=gtdict[i]) for i,s in self.snps.items()]
-		gtsd = dict(zip([gt.ID for gt in gts],gts))
-		for gt1 in self.hla:
-			for gt2 in self.hla[gt1]:
-				if gt1 in gtsd and gt2 in gtsd:
-					wsum += self.hla[gt1][gt2]
-		return (wsum / self.N)
+#################################################
+#
+# --%%  CLASS: MultiRiskScore  %%--
+
+class MultiRiskScore(RiskScore):
+	"""Calculate GRS based on MultiLocus Weights."""
+	def __init__(self, snps, risks, multirisks, *args, **kwargs):
+		super().__init__(snps=snps, risks=risks, *args, **kwargs)
+		self.multi = self.ReadMultiRisk(multirisks)
 
 	def calc(self, gtdict, **kwargs):
+		"""INPUT: Gtdict => dict {str(id):GenoType}; RETURN: A risk score (float)"""
 		wsum = super().calc(gtdict, **kwargs)
-		return wsum + self.calc_oramhla(gtdict)
+		for gt1 in self.multi:
+			for gt2 in self.multi[gt1]:
+				if gt1 in gtdict.values() and gt2 in gtdict.values():
+					wsum += self.multi[gt1][gt2] / self.N
+		return wsum
 
-	def hlaweights(self, factors=None):
-		import collections
-		dict_dicts = collections.defaultdict(lambda: collections.defaultdict(float))
-		if isinstance(factors, str):
-			with open(factors) as f:
-				for line in csv.DictReader(f):
-					beta = float(line.get("BETA", math.log(float(line.get("ODDSRATIO")))))
-					gt1 = pksnps.GenoType(CHROM=line.get("CHROM_1"), POS=line.get("POS_1"), hhh=line.get("ID_1"), genotype=line.get("GENOTYPE_1"))
-					gt2 = pksnps.GenoType(CHROM=line.get("CHROM_2"), POS=line.get("POS_2"), hhh=line.get("ID_2"), genotype=line.get("GENOTYPE_2"))
-					dict_dicts[gt1][gt2] = beta
-		else:
-			return NotImplemented
+	@staticmethod
+	def ReadMultiRisk(riskfobj):
+		"""We should probably write this with arbitrary nesting... It's not completely finished and is therefore intentionally dirty"""
+		dict_dicts = collections.defaultdict(dict)
+		riskiter = csv.DictReader(riskfobj)
+		for risk in riskiter:
+			beta = float(risk.get("BETA", math.log(float(risk.get("ODDSRATIO")))))
+			gt = []
+			while True:
+				i = len(gt) + 1
+				chrom = risk.get("CHROM_" + str(i), risk.get("POSID_" + str(i),":").split(":")[0])
+				pos   = risk.get("POS_" + str(i), risk.get("POSID_" + str(i),":").split(":")[1])
+				try: gt.append(pksnps.GenoType(CHROM=chrom, POS=int(pos), genotype=risk.get("GENOTYPE_" + str(i), "").split(":")))
+				except AttributeError as ae: 
+					print("\n" + str(ae), file=sys.stderr)
+					sys.exit("Read Error: Each line of '" + str(riskfobj.name) + "' must contain one weight and at least two recognizable positions and alleles.\n")
+				if len(gt) >= 2:
+					break
+			dict_dicts[gt[0]][gt[1]] = beta
 		return dict_dicts
 
 
-class sharp2016(RiskScore):
-	def weights():
-		with open(str(pathlib.Path(__file__).resolve().parent.absolute()) + "/sharp2019.weights.txt") as f:
-			snptable = csv.DictReader(f)
-			return list(csv.DictReader(f))
 
-	def __init__(self, alt_allele=dict(), factors=weights(), **kwargs):
-		super().__init__(alt_allele=alt_allele, factors=factors, **kwargs)
+
+#################################################
+#
+# --%%  CLASS: Oram2016  %%--
+
+class oram2016(MultiRiskScore):
+	"""Calculate GRS based on Oram et al 2016"""
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.N = 2 * (self.N + 1)
+
+
+
+#################################################
+#
+# --%%  CLASS: Shapr2019  %%--
+
+class sharp2019(MultiRiskScore):
+	"""Calculate GRS based on Sharp et al 2019"""
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 		self.N = 1
 
-	def calc(self, subject):
+	def calc(self, gtdict, **kwargs):
 #	sharp is in three parts (int / no-int) + others
+		wsum = super().calc(gtdict, **kwargs)
+		return wsum + self.calc_sharp(gtdict)
+
+	def calc_sharp(self, gtdict):
+		"""For haplotypes with an interaction the beta is taken from Table S3, without an interaction it is scored independently for each haplotype of the pair. This last part mustm be done here."""
 		wsum = 0
-		print(wsum)
+		return wsum / self.N
 
 
--- riskscore.py
+++ riskscore.py
@@ -1,7 +1,7 @@
 #!/home/fls530/anaconda3/bin/python
 
 #
-#  risk score ???
+#  riskscore.py
 #
 #  Copyright 2019 Carsten Friis Rundsten <fls530@ku.dk>
 #
@@ -25,23 +25,137 @@
 
 # Notes and TODOs:
 
+# TODO: Thees a couple of functions which could be cleaned at the bottom
+# TODO: There's still some mess in how we match weights => Allele, GenoType, dosage 
+# TODO: Here's a funny bug. If the info file is a csv, then the hla part of oram fails...
 
 
 ##################################################
 #
 # --%%  RUN: Perform Basic Setup  %%--
 
-Version = "0.1 (Development Version)"
+Version = "0.6 (Development Version)"
 
-import argparse
+import click
+import pathlib
 import pkcsv as csv
+import pkclick
 import pksnps as snps
 import pkrs as riskscore
+import re
 import sys
  
 # --%%  END: Perform Basic Setup  %%--
 #
 ##################################################
+
+EPILOG_FileFormat = """
+\b
+Column-Based Datafiles:
+
+Several options accept files containing data in tables/columns. Columns separators are auto-detected from the input and should work for tab-separated files, comma-seperated (csv) files, and space separated files. 
+The file must have one and only one headline as columns are identified by name. Any column name not recognized is ignored.
+
+\b
+Recognized names are:
+ALLELE    - Identification of the weighted allele. Usually given as a nucleotide. 
+BETA      - The weight to be used. Used unmodified. Takes precedent over 'ODDSRATIO'.
+CHROM     - Chromosome name or number.
+ODDSRATIO - Ignored if column 'BETA' is given. Otherwise the natural logarithm of ODDSRATIO will be used as weights.
+POS       - Chromosomal Position.
+POSID     - An identifier of the type CHR:POS.
+
+"""
+
+OPTION_geno = """
+Geno file of the type created by SNPextractor
+"""
+
+OPTION_info = """
+Info file of the type created by SNPextractor
+"""
+
+OPTION_vcf = """
+Load VCF File using PyVCF VCFv4.0 and 4.1 parser for Python. Note that this option requires reading the entire VCF into memory, which is not
+recommended for large VCF files. Use 'bcftools view --regions' or similar, to first reduce large files in size.
+"""
+
+OPTION_weights = """
+Single locus risk weights file. For accepted file formats, see description on 'Column-Based Datafiles'
+"""
+
+##################################################
+#
+# --%%  RUN: Commands  %%--
+
+class StdCommand(click.Command):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.params.insert(0, click.Option(('-i','--info',), type=click.File(), help=OPTION_info))
+		self.params.insert(0, click.Option(('-g','--geno',), type=click.File(), help=OPTION_geno))
+		self.epilog = EPILOG_FileFormat
+
+@click.group()
+@click.version_option(version=Version)
+def main():
+	"""THIS SCRIPT IS STILL EXPERIMENTAL; USE WITH CAUTION"""
+	pass
+
+@main.command(cls=StdCommand)
+@click.option('-w','--weights', type=click.File(), default=None,
+              help="Single locus risk weights file.")
+def aggregate(geno, info, weights):
+	"""Calculate Aggregate Gene Risk Score from user-provided weights."""
+	check_args(geno, info, weights)
+	snpinfo = snps.ReadInfo(info)
+	sbjgeno = snps.ReadGeno(geno, snpinfo)
+	grs = riskscore.RiskScore(snpinfo, risks=weights)
+	for sbjid,gt in sbjgeno.items():
+		print(sbjid + "\t" + str(grs.calc(gt)))
+
+@main.command(cls=StdCommand)
+@click.option('-m','--multilocus',  type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights.multilocus.txt", 
+              help="Multilocus risk weights.")
+@click.option('-w','--weights', type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights.txt", help=OPTION_weights)
+def oram2016(geno, info, weights, multilocus):
+	"""Calculate Gene Risk Score based on Oram et al 2016."""
+	check_args(geno, info, weights, multilocus)
+	snpinfo = snps.ReadInfo(info)
+	sbjgeno = snps.ReadGeno(geno, snpinfo)
+	grs = riskscore.oram2016(snpinfo, risks=weights, multirisks=multilocus)
+	for sbjid,gt in sbjgeno.items():
+		print(sbjid + "\t" + str(grs.calc(gt)))
+
+@main.command(cls=StdCommand)
+@click.option('-m','--multilocus',  type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/sharp2019.weights.multilocus.txt", 
+              help="Multilocus risk weights.")
+@click.option('-w',"--weights", type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/sharp2019.weights.txt", help=OPTION_weights)
+def sharp2019(geno, info, weights, multilocus):
+	"""Calculate Gene Risk Score based on Sharp et al 2019. (DO NOT USE: UNDER DEVELOPMENT)."""
+	check_args(geno, info, weights, multilocus)
+	snpinfo = snps.ReadInfo(info)
+	grs = riskscore.sharp2019(snpinfo, risks=weights, multirisks=multilocus)
+	process_geno(geno, grs)
+
+@main.command()
+@click.option('--vcf', type=pkclick.gzFile(mode='rb'), help=OPTION_vcf)
+@click.option('-m','--multilocus',  type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights.multilocus.txt", 
+              help="Multilocus risk weights.")
+@click.option('-w','--weights', type=click.File(), default=str(pathlib.Path(__file__).resolve().parent.absolute()) + "/oram2016.weights.txt", help=OPTION_weights)
+def test(vcf, weights, multilocus):
+	"""FOR TESTING PURPOSES ONLY; DO NOT USE!"""
+	import vcf as vcf_
+	vcfdata = snps.ReadVCF(vcf_.Reader(vcf), drop_genotypes=False)
+	sbjgeno = transpose_geno([snp.genotype() for snp in vcfdata.values()])
+	snpinfo = [snp.drop_genotype() for snp in vcfdata.values()]
+	grs = riskscore.oram2016(snpinfo, risks=weights, multirisks=multilocus)
+	for subjectid, gt in sbjgeno.items():
+		print(subjectid + "\t" + str(grs.calc(gt)))
+
+# --%%  END: Commands  %%--
+#
+##################################################
+
 
 
 
@@ -49,69 +163,35 @@ import sys
 #
 # --%%  RUN: Subroutines  %%--
 
-#def showversion():
-#	print(Version)
-#	sys.exit(0)
+def check_args(geno, info, *args):
+	if not (geno and info):
+		sys.exit("ERROR: No input files specified, add both '--geno' and '--info' options.")
+	return 1
 
-def get_algorithm(args):
-	snpinfo = snps.ReadInfo(args.info)
-	if args.oram2016:
-		grs = riskscore.oram2016(snpinfo)
-	else:
-		sys.exit("Algorithm Error!")
-	return grs
+# Another quick'n'dirty function which should probably be done cleaner
+def transpose_geno(genoiter):
+	import collections
+	dict_dicts = collections.defaultdict(dict)
+	for geno_dict in genoiter:
+		for sbjid, gt in geno_dict.items():
+			dict_dicts[sbjid][gt.ID] = gt
+	return dict_dicts
+
+# Should probably write an actual parser function which converts geno to instances of my genotype class...
+def process_geno(geno, grs):
+	for gtype in csv.DictReader(geno):
+		subjectid = gtype.pop("")
+		for k in gtype:
+			gtype[k] = sum(float(x) for x in re.split("[/|]+", gtype[k])) 
+		print(subjectid + "\t" + str(grs.calc(gtype)))
 
 
-
-##################################################
+# --%%  END: Subroutines  %%--
 #
-# --%%  RUN: Main Program  %%--
+##################################################
 
-# Setup ArgParse Here...
-
-def setup_args():
-	parser = argparse.ArgumentParser(description="THIS SCRIPT IS STILL EXPERIMENTAL; USE WITH CAUTION",
-	                         epilog="Fisk")
-	parser.add_argument('-g', '--geno', default=None,
-		help="Geno file")
-	parser.add_argument('-i', '--info', default=None,
-		help="Info file")
-	parser.add_argument('--oram2016', const="risk_score/oram2016.weights_hla.txt", default="", nargs="?",
-		help="Oram et al 2016")
-	parser.add_argument('-v', '--version', action='version', version=Version, help="Print Version and exit.")
-# choice of algorithm
-# optional weights file
-	args = parser.parse_args()
-	if not (args.geno or args.info):
-		parser.error('No input files specified, add --geno or --info (or both)')
-	args.info = args.info if args.info else [args.geno.replace(".geno.",".info.")]
-#	if args.version:
-#		showversion()
-	return args
-
-
-# Execute Main Program from Here
-
-def main(args):
-	grs = get_algorithm(args)
-
-# loop over input
-	with open(args.geno) as f:
-		for geno in csv.DictReader(f):
-			subjectid = geno.pop("")
-			print(subjectid + "\t" + str(grs.calc(geno)))
-	return 0
 
 if __name__ == '__main__':
-	args = setup_args()
-	state = main(args)
-	sys.exit(state)
-
-# --%%  END: Main program  %%--
-#
-##################################################
-
-
-
+	main()
 
 
